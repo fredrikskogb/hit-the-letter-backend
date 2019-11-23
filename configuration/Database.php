fffe31700eb053a4f526be7cbf723971d7c6e3d1
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2019-11-23 20:09:12 +0100

    init commit, set up boiler plate / structure

---
+++ configuration/Database.php
@@ -0,0 +1,20 @@
+<?php 
+	class Database {
+    
+	private $host = 'localhost';
+	private $username = 'root';
+	private $dbName = 'hit_the_letter';
+	private $password = 'root';
+	private $connection;
+	// DB Connect
+	public function connect() {
+		$this->connection = null;
+		try { 
+			$this->connection = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbName, $this->username, $this->password);
+			$this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch(PDOException $e) {
+			echo 'Connection Error: ' . $e->getMessage();
+		}
+		return $this->connection;
+	}	
+}
