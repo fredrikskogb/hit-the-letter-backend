aa04632e772532451f7c7ff22539d87d9724f039
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2019-11-25 22:07:29 +0100

    temporary login and register working

--- api/user/register.php
+++ api/user/register.php
@@ -12,9 +12,9 @@ $db = $database->getConnection();
 $user = new User($db);
  
 // set user property values
-$user->username = $_POST['email'];
-$user->password = base64_encode($_POST['password']);
-//$user->created = date('Y-m-d H:i:s'); needed?
+$user->email = $_GET['email'];
+$user->password = base64_encode($_GET['password']);
+$user->created = date('Y-m-d H:i:s');
  
 // create the user
 if($user->signup()){
@@ -22,13 +22,13 @@ if($user->signup()){
         "status" => true,
         "message" => "Successfully Signup!",
         "id" => $user->id,
-        "username" => $user->username
+        "email" => $user->email
     );
 }
 else{
     $user_arr=array(
         "status" => false,
-        "message" => "Username already exists!"
+        "message" => "Email already exists!"
     );
 }
 print_r(json_encode($user_arr));
