5427b767e309091e1a2ca175a49222a467646567
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2019-12-02 01:29:30 +0100

    added api for highscore

---
+++ api/highscore/getHighscore.php
@@ -0,0 +1,38 @@
+<?php
+// include database and object files
+include_once '../../configuration/Database.php';
+include_once '../../models/Highscore.php';
+
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+ 
+// prepare user object
+$highscore = new Highscore($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+// set ID property of user to be edited
+$highscore->user_id = $data->userId;
+$highscore->level = $data->level;
+$highscore->points = $data->points;
+
+if($highscore->getHighscore()){
+    // get retrieved row
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    // create array
+    $highscore_arr=array(
+        "status" => true,
+        "id" => $row['user_id'],
+        "email" => $row['points']
+    );
+}
+else{
+    $highscore_arr=array(
+        "status" => false,
+        "message" => "Error",
+    );
+}
+// make it json format
+print_r(json_encode($highscore_arr));
+?>
